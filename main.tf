80c6e595de0a2df36f570245d6be6a1064a601ff
diego <dsrocha00@gmail.com>
2023-07-01 17:04:45 -0300

    acrescentando vnet

--- main.tf
+++ main.tf
@@ -20,10 +20,23 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
 resource   "azurerm_windows_virtual_machine"   "example"   {
   name                    =   "myvm1"
-  location                =   "northeurope"
+  location                =   azurerm_resource_group.rg.location
   resource_group_name     =   azurerm_resource_group.rg.name
   network_interface_ids   =   []
   size                    =   "Standard_B1s"
