d4432af0e98622a5b23b9aec961e575a6ccd2cf4
diego <dsrocha00@gmail.com>
2023-07-01 17:01:49 -0300

    implementando maquina virtual

--- main.tf
+++ main.tf
@@ -18,3 +18,27 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-moead"
   location = "westus2"
 }
+
+
+
+resource   "azurerm_windows_virtual_machine"   "example"   {
+  name                    =   "myvm1"
+  location                =   "northeurope"
+  resource_group_name     =   azurerm_resource_group.rg.name
+  network_interface_ids   =   []
+  size                    =   "Standard_B1s"
+  admin_username          =   "adminuser"
+  admin_password          =   "Password123!"
+
+  source_image_reference   {
+    publisher   =   "MicrosoftWindowsServer"
+    offer       =   "WindowsServer"
+    sku         =   "2019-Datacenter"
+    version     =   "latest"
+  }
+
+  os_disk   {
+    caching             =   "ReadWrite"
+    storage_account_type   =   "Standard_LRS"
+  }
+}
