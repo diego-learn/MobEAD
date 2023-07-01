f9f4d2f86ac696d2aa26828e19b56deb029e41c6
diego <dsrocha00@gmail.com>
2023-07-01 17:08:23 -0300

    ajustes vnet

--- main.tf
+++ main.tf
@@ -34,11 +34,23 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource   "azurerm_windows_virtual_machine"   "example"   {
   name                    =   "myvm1"
   location                =   azurerm_resource_group.rg.location
   resource_group_name     =   azurerm_resource_group.rg.name
-  network_interface_ids   =   []
+  network_interface_ids   =   [ azurerm_network_interface.example.id,]
   size                    =   "Standard_B1s"
   admin_username          =   "adminuser"
   admin_password          =   "Password123!"
